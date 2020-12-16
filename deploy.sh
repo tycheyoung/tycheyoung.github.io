50cbdce13f98e86029d30c0944b9aa7d11f622ac
tycheyoung <retbird13@gmail.com>
2020-12-16 10:27:22 -0800

    Fix restore script issue

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
 hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
@@ -10,10 +13,11 @@ cd public
 # Add changes to git.
 git add .
 
+
 # Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
 fi
 git commit -m "$msg"
 
--- restore.sh
+++ restore.sh
@@ -5,6 +5,7 @@ echo "Prior to this, make sure to add new SSH key in your GitHub"
 
 rm -rf public
 git rm --cached public
+git submodule add -f -b master https://github.com/tycheyoung/tycheyoung.github.io.git public
 
 # If error persists, check .git/config and add public related things
 git submodule update
