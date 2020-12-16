f9229bcee686aa3ccfee318b19aab1569da8c625
tycheyoung <retbird13@gmail.com>
2020-12-16 10:32:24 -0800

    Fix restore script

--- restore.sh
+++ restore.sh
@@ -6,8 +6,10 @@ echo "Prior to this, make sure to add new SSH key in your GitHub"
 rm -rf public
 git rm --cached public
 git submodule add -f -b master https://github.com/tycheyoung/tycheyoung.github.io.git public
+git submodule add https://github.com/MunifTanjim/minimo themes/minimo
 
 # If error persists, check .git/config and add public related things
+git submodule init
 git submodule update
 git submodule foreach git checkout master
 
