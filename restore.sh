1e39ef9ecfdab8d8873b61fccf4b406602a3e229
tycheyoung <retbird13@gmail.com>
2024-01-20 17:59:08 -0800

    update

--- restore.sh
+++ restore.sh
@@ -3,9 +3,6 @@
 echo "Use this if you restored in the new environment"
 echo "Prior to this, make sure to add new SSH key in your GitHub"
 
-rm -rf public
-git rm --cached public
-git submodule add -f -b master git@github.com:tycheyoung/tycheyoung.github.io.git public
 git submodule add git@github.com:MunifTanjim/minimo.git themes/minimo
 
 # If error persists, check .git/config and add public related things
